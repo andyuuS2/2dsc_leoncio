7616950cf2a11237b259d0ddbc001af26e96d5cb
andyuuS2 <maria.e.souza1509@gmail.com>
2024-10-03 15:21:57 +0000

    INICIADO O COD JS

---
+++ calculadora/js/script.js
@@ -0,0 +1,17 @@
+//FAZ REF AOS ELEMENT DOM
+const numero1 = document.querySelector('#numero1')
+const numero2 = document.querySelector('#numero2')
+const btnCalcular = document.querySelector('#btn-calcular')
+const resultado = document.querySelector('#resultado')
+const soma = document.querySelector('#soma')
+const  divisao = document.querySelector('#divisao')
+const  subtracao= document.querySelector('#subtracao')
+const  multiplicacao= document.querySelector('#multiplicacao')
+
+
+
+alert(numero1)
+
+//CRIA A FUNÇÃO CALCULAR
+
+//ADICIONA ESCUTAR DE ELEMENTOS
