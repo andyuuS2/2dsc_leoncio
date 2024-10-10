a65fe434d55676547ee5bf83e2bc79efa2ac5f80
andyuuS2 <maria.e.souza1509@gmail.com>
2024-10-10 15:25:45 +0000

    CALCULADORA FUNCIONANDO

--- calculadora/js/script.js
+++ calculadora/js/script.js
@@ -10,8 +10,23 @@ const  multiplicacao= document.querySelector('#multiplicacao')
 
 
 
-alert(numero1)
+//alert(numero1)
 
 //CRIA A FUNÇÃO CALCULAR
+function calcular() {
+    const n1 = Number (numero1.value)
+    const n2 = Number (numero2.value)
+    if (typeof n1 === 'number'
+        && typeof n2 === 'number') {
+            const adicao = `A soma de ${n1} e ${n2} = ${n1+n2}`
+            alert(adicao)
+        } else {
+            alert('Por favor, digite um numero correto')
+        }
+    //alert('Resultado: ' + resultado)
+  }
 
 //ADICIONA ESCUTAR DE ELEMENTOS
+btnCalcular.addEventListener('click',function(){
+    calcular()
+})
