97d5e2b904a7fad2caa4ee332f4a3f6e30e101ee
andyuuS2 <maria.e.souza1509@gmail.com>
2024-10-17 15:19:46 +0000

    FFUNCIONAMENTO JS

--- calculadora/js/script.js
+++ calculadora/js/script.js
@@ -3,10 +3,10 @@ const numero1 = document.querySelector('#numero1')
 const numero2 = document.querySelector('#numero2')
 const btnCalcular = document.querySelector('#btn-calcular')
 const resultado = document.querySelector('#resultado')
-const soma = document.querySelector('#soma')
-const  divisao = document.querySelector('#divisao')
-const  subtracao= document.querySelector('#subtracao')
-const  multiplicacao= document.querySelector('#multiplicacao')
+const soma = document.querySelector('.soma')
+const  divisao = document.querySelector('.divisao')
+const  subtracao= document.querySelector('.subtracao')
+const  multiplicacao= document.querySelector('.multiplicacao')
 
 
 
@@ -18,8 +18,17 @@ function calcular() {
     const n2 = Number (numero2.value)
     if (typeof n1 === 'number'
         && typeof n2 === 'number') {
-            const adicao = `A soma de ${n1} e ${n2} = ${n1+n2}`
-            alert(adicao)
+            const add = `A soma de ${n1} e ${n2} = ${n1+n2}`
+            const sub = `A soma de ${n1} e ${n2} = ${n1-n2}`
+            const div = `A soma de ${n1} e ${n2} = ${n1*n2}`
+            const mult = `A soma de ${n1} e ${n2} = ${n1/n2}`
+
+            soma.innerHTML = add
+            sub.innerHTML = sub
+            div.innerHTML = div
+            mult.innerHTML = mult
+
+            
         } else {
             alert('Por favor, digite um numero correto')
         }
@@ -27,6 +36,7 @@ function calcular() {
   }
 
 //ADICIONA ESCUTAR DE ELEMENTOS
-btnCalcular.addEventListener('click',function(){
+btnCalcular.addEventListener('click',function(evento){
+    evento.preventDefault()
     calcular()
 })
